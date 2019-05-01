835d59036b9557deb9e3d9644a293505bbf40705
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-05-01 16:31:01 -0700

    Add S3 bundle

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/earnings/:query', express.static(path.join(__dirname, 'public')));
 app.locals.newrelic = newrelic;
 
 app.get('/api/earnings/:query', (req, res) => {
