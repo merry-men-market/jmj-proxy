f2b2d7a03e5bf6940ad5107ccf0f6cc052b40246
Jeena Chemparathy <jeena@jeenas-mbp.lan>
2019-05-02 08:30:46 -0700

    Remove morgan

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const newrelic = require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const path = require('path');
 const axios = require('axios');
 
